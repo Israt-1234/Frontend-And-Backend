bde07c82ac585f9cc75de9a3571d185c54789566
Israt Jahan <140290748+Israt-1234@users.noreply.github.com>
2024-01-20 14:28:17 +0600

    Create style.js

---
+++ style-block/style.js
@@ -0,0 +1,10 @@
+document.getElementById('style1').addEventListener('click', style);
+
+function style() {
+    document.getElementById('demo').style.fontSize = '50px';
+}
+
+document.getElementById('block1').addEventListener('click',  function () {
+    document.getElementById('demo2').style.display = 'block';
+}
+);
