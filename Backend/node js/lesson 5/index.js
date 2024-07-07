a6241eea18a9ad3598a450b7623e91ae712e1d7f
Israt Jahan <140290748+Israt-1234@users.noreply.github.com>
2024-07-07 04:11:03 +0000

    external module and npm

---
+++ Backend/node js/lesson 5/index.js
@@ -0,0 +1,8 @@
+const getRandomFruitsName = require('random-fruits-name')
+console.log(getRandomFruitsName())  //apple
+console.log(getRandomFruitsName('en'));
+console.log(getRandomFruitsName('es'));
+
+
+//npm init -y
+//npm install random-fruits-name 
